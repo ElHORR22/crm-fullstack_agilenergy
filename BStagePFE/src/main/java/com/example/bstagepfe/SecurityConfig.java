cdc48ae6bac2448a998cfc3fce870173468e30e7
Haroun Dorbez <dorbezaaron@gmail.com>
2026-05-12 02:54:28 +0100

    Update SecurityConfig.java

--- BStagePFE/src/main/java/com/example/bstagepfe/SecurityConfig.java
+++ BStagePFE/src/main/java/com/example/bstagepfe/SecurityConfig.java
@@ -53,7 +53,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+    "http://localhost:4200",
+    "https://crm-fullstackagilenergy-production.up.railway.app",
+    "https://crm-agil-energy.vercel.app" 
+));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of(
                 "Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"
